4a61e862192c9119ace72c819fa5b9dd091985f4
ulyssetsd <utassidis@gmail.com>
2017-11-05 16:07:14 +0800

    SQL create for users in file sql.py, add of psycopg2

--- flaskr/flaskr/sql.py
+++ flaskr/flaskr/sql.py
@@ -1,11 +1,23 @@
 # -*- coding: utf-8 -*-
 from database import conn, cursor
 
+sql = "CREATE TABLE users ( " + \
+      "user_id serial primary key not null, " + \
+      "email text, " + \
+      "password text, " + \
+      "nickname text, " + \
+      "c_time timestamp );"
+cursor.execute(sql)
+conn.commit()
+
 sql = "CREATE TABLE message ( " + \
       "msg_id serial primary key not null, " + \
       "user_id int not null, " + \
       "content text , " + \
-      "c_time timestamp );"
+      "c_time timestamp , " + \
+      "status integer );"
+cursor.execute(sql)
+conn.commit()
 
 sql = "CREATE TABLE comment ( " + \
       "cmt_id serial primary key not null, " + \
@@ -13,12 +25,16 @@ sql = "CREATE TABLE comment ( " + \
       "user_id int not null, " + \
       "content text , " + \
       "c_time timestamp );"
+cursor.execute(sql)
+conn.commit()
 
 sql = "CREATE TABLE like_msg ( " + \
       "like_msg_id serial primary key not null, " + \
       "msg_id int not null, " + \
       "user_id int not null, " + \
       "c_time timestamp);"
+cursor.execute(sql)
+conn.commit()
 
 sql = "CREATE TABLE like_cmt ( " + \
       "like_cmt_id serial primary key not null, " + \
@@ -27,3 +43,4 @@ sql = "CREATE TABLE like_cmt ( " + \
       "c_time timestamp);"
 cursor.execute(sql)
 conn.commit()
+print('database updated!')
