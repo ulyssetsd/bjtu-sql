6db6379c9e474bce6470db021310256c151e7fe4
ulyssetsd <utassidis@gmail.com>
2017-11-26 14:49:02 +0800

    add of the requete file and unit test

---
+++ flaskr/flaskr/requete.py
@@ -0,0 +1,109 @@
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+    render_template, flash
+
+from flaskr.helpers import conn, cursor
+
+def userGetAll():
+	cursor.execute("SELECT * FROM users")
+	users = cursor.fetchall()
+	return users
+
+def userIdByEmailPassword(email, password):
+	cursor.execute("SELECT user_id FROM users WHERE email = %s AND password = crypt(%s, password);", (email, password,))
+	u = cursor.fetchone()
+	if u is None:
+		return None
+	return u[0]
+
+def userByEmail(email):
+	cursor.execute("SELECT * FROM users where email = %s;", (email,))
+	u = cursor.fetchone()
+	return u
+
+def userCreate(email, nickname, password):
+	cursor.execute("INSERT INTO users(email,nickname,password) VALUES(%s,%s,crypt(%s, gen_salt('bf', 8)));", (email, nickname, password,))
+	conn.commit()
+
+def userDelete(email):
+	cursor.execute("DELETE FROM users WHERE email = %s;", (email,))
+	conn.commit()
+
+def userUpdateNicknameByEmail(nickname, email):
+	cursor.execute("UPDATE users SET nickname = %s where email = %s", (nickname, email,))
+	conn.commit()
+
+def userUpdatePasswordByEmail(password, email):
+	cursor.execute("UPDATE users SET password = crypt(%s, gen_salt('bf', 8)) where email = %s", (password, email,))
+	conn.commit()
+
+
+
+
+
+
+def messageGetAll():
+	cursor.execute('SELECT * FROM message')
+	message = cursor.fetchall()
+	return message
+
+def messageCreate(user_id, content, c_time):
+	cursor.execute("INSERT INTO message(user_id,content,c_time) VALUES(%s,%s,%s);", (user_id, content, c_time,))
+	conn.commit()
+
+def messageDeleteByUserId(user_id):
+	cursor.execute("DELETE FROM message where user_id = %s;", (user_id,))
+	conn.commit()
+
+def messageGetAllFromUserIdOrder(user_id):
+	cursor.execute('SELECT * FROM message where user_id = %s ORDER BY c_time DESC', (user_id,))
+	m = cursor.fetchall()
+	return m
+
+def messageDeleteById(msg_id):
+	cursor.execute("DELETE FROM message where msg_id = %s;", (msg_id,))
+	conn.commit()
+
+def messageById(msg_id):
+	cursor.execute("SELECT * FROM message where msg_id = %s;", (msg_id,))
+	m = cursor.fetchone()
+	return m
+
+def messageGetAllFromUserId(user_id):
+	cursor.execute('SELECT * FROM message WHERE user_id = %s;', (user_id,))
+	message = cursor.fetchall()
+	return message
+
+def messageUpdateContentById(content, msg_id):
+	cursor.execute("UPDATE message SET content = %s where msg_id = %s;", (content, msg_id,))
+	conn.commit()
+
+def messageCount():
+	cursor.execute("SELECT COUNT(*) AS count FROM message")
+	nb = cursor.fetchone()
+	if nb is None:
+		return 0
+	return nb['count']
+
+
+
+
+
+def likeMsgGetAll():
+	cursor.execute('SELECT * FROM like_msg')
+	message = cursor.fetchall()
+	return message
+
+def likeMsgCreate(msg_id, user_id, c_time):
+	cursor.execute("INSERT INTO like_msg(msg_id, user_id,c_time) VALUES(%s,%s,%s);", (msg_id, user_id, c_time,))
+	conn.commit()
+
+def likeMsgDelete(msg_id, user_id):
+	cursor.execute("DELETE FROM like_msg where msg_id = %s AND user_id = %s;", (msg_id, user_id,))
+	conn.commit()
+
+def likeMsgCount():
+	cursor.execute("SELECT COUNT(*) AS count FROM like_msg")
+	nb = cursor.fetchone()
+	if nb is None:
+		return 0
+	return nb['count']
---
+++ flaskr/flaskr_tests.py
@@ -0,0 +1,290 @@
+import os
+import flaskr
+import unittest
+import tempfile
+import sys
+from datetime import datetime
+
+# import flaskr.database
+# from flaskr.flaskr2 import db_getEntries2
+from flaskr.requete import *
+
+
+class FlaskrTestCase(unittest.TestCase):
+
+	debug = True
+	emailGeneral = "toto@fleur.com"
+	idGeneral = -69
+
+	def setUp(self):
+		self.db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
+		flaskr.app.testing = True
+		self.app = flaskr.app.test_client()
+		with flaskr.app.app_context():
+			self.ptrMessageGetAll = messageGetAll
+			self.ptrUserGetAll = userGetAll
+			self.ptrMessageCount = messageCount
+			self.ptrUserByEmail = userByEmail
+			self.ptrUserCreate = userCreate
+			self.ptrUserDelete = userDelete
+			self.ptrUserIdByEmailPassword = userIdByEmailPassword
+			self.ptrUserUpdateNicknameByEmail = userUpdateNicknameByEmail
+			self.ptrUserUpdatePasswordByEmail = userUpdatePasswordByEmail
+			self.ptrMessageCreate = messageCreate
+			self.ptrMessageDeleteByUserId = messageDeleteByUserId
+			self.ptrMessageGetAllFromUserId = messageGetAllFromUserId
+			self.ptrMessageById = messageById
+			self.ptrMessageUpdateContentById = messageUpdateContentById
+			self.ptrMessageDeleteById = messageDeleteById
+			self.ptrMessageGetAllFromUserIdOrder = messageGetAllFromUserIdOrder
+			self.ptrLikeMsgCreate = likeMsgCreate
+			self.ptrLikeMsgCount = likeMsgCount
+			self.ptrLikeMsgGetAll = likeMsgGetAll
+			self.ptrLikeMsgDelete = likeMsgDelete
+
+	def tearDown(self):
+		os.close(self.db_fd)
+		os.unlink(flaskr.app.config['DATABASE'])
+
+	def test_messageGetAll(self):
+		self.display('test_messageGetAll', self.ptrMessageGetAll())
+
+	def test_usersGetAll(self):
+		self.display('test_userGetAll', self.ptrUserGetAll())
+
+	def test_likeMsgGetAll(self):
+		self.display('test_likeMsgGetAll', self.ptrLikeMsgGetAll())
+
+	def test_messageCount(self):
+		u = self.ptrMessageCount()
+		self.ptrMessageCreate(self.idGeneral, 'Bite', datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+		u1 = self.ptrMessageCount()
+		self.ptrMessageDeleteByUserId(self.idGeneral)
+		if u == (u1 - 1):
+			self.display('test_messageCount', "")
+			return ""
+		self.display('test_messageCount', 'Erreur perso...', 1)
+
+	def test_userByEmail(self):
+		emailTest = self.emailGeneral
+
+		self.cleanEmail(emailTest)
+		u = self.ptrUserByEmail(emailTest)
+		if u is None:
+			# User NONE
+			self.ptrUserCreate(emailTest, "jean", "pierrePaul")
+			# User exist
+			u = self.ptrUserByEmail(emailTest)
+			if u is None:
+				self.display('test_userByEmail', 'Erreur perso...', 1)
+				return ''
+		self.display('test_userByEmail', "")
+		userDelete(emailTest)
+
+	def test_userCreate(self):
+		emailTest = self.emailGeneral
+
+		self.cleanEmail(emailTest)
+		u = self.ptrUserByEmail(emailTest)
+		if u is not None:
+			userDelete(emailTest)
+		# User NONE
+		self.ptrUserCreate(emailTest, "jean", "pierrePaul")
+		# User exist
+		u = self.ptrUserByEmail(emailTest)
+		if u is None:
+			self.display('test_userCreate', 'Erreur perso...', 1)
+			return ''
+		self.display('test_userCreate', "")
+		userDelete(emailTest)
+
+	def test_userDelete(self):
+		emailTest = self.emailGeneral
+
+		self.cleanEmail(emailTest)
+		u = self.ptrUserByEmail(emailTest)
+		if u is None:
+			# User NONE
+			self.ptrUserCreate(emailTest, "jean", "pierrePaul")
+			# User exist
+		u = self.ptrUserByEmail(emailTest)
+		if u is not None:
+			self.ptrUserDelete(emailTest)
+			u = self.ptrUserByEmail(emailTest)
+			if u is None:
+				self.display('test_userDelete', "")
+				return ""
+		self.display('test_userDelete', 'Erreur perso...', 1)
+	
+	def test_userIdByEmailPassword(self):
+		emailTest = self.emailGeneral
+
+		self.cleanEmail(emailTest)
+		self.ptrUserCreate(emailTest, "jean", "pierrePaul")
+		userSave = self.ptrUserByEmail(emailTest)
+		userId = self.ptrUserIdByEmailPassword(emailTest, "pierrePaul")
+		if userId == userSave[0]:
+			self.display('test_userIdByEmailPassword', "")
+			return ""
+		self.display('test_userIdByEmailPassword', 'Erreur perso...', 1)
+
+	def test_userUpdateNicknameByEmail(self):
+		emailTest = self.emailGeneral
+
+		self.cleanEmail(emailTest)
+		self.ptrUserCreate(emailTest, "jean", "pierrePaul")
+		userSave = self.ptrUserByEmail(emailTest)
+		self.ptrUserUpdateNicknameByEmail('thomas', emailTest)
+		userSave2 = self.ptrUserByEmail(emailTest)
+		if userSave2[1] == userSave[1]:
+			self.display('test_userUpdateNicknameByEmail', "")
+			return ""
+		self.display('test_userUpdateNicknameByEmail', 'Erreur perso...', 1)
+
+	def test_userUpdatePasswordByEmail(self):
+		emailTest = self.emailGeneral
+
+		self.cleanEmail(emailTest)
+		self.ptrUserCreate(emailTest, "jean", "pierrePaul")
+		userSave = self.ptrUserByEmail(emailTest)
+		self.ptrUserUpdatePasswordByEmail("monNewMdp", emailTest)
+		userSave2 = self.ptrUserByEmail(emailTest)
+		#print(userSave, userSave2)
+		if userSave2[2] != userSave[2]:
+			self.display('test_userUpdatePasswordByEmail', "")
+			return ""
+		self.display('test_userUpdatePasswordByEmail', 'Erreur perso...', 1)
+
+	def test_messageCreate(self):
+		u = self.ptrMessageCount()
+		self.ptrMessageCreate(self.idGeneral, 'Bite', datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+		u1 = self.ptrMessageCount()
+		if u < u1:
+			self.display('test_messageCreate', "")
+			self.ptrMessageDeleteByUserId(self.idGeneral)
+			return ""
+		self.display('test_messageCreate', 'Erreur perso...', 1)
+
+	def test_messageDeleteByUserId(self):
+		self.ptrMessageCreate(self.idGeneral, 'Bite', datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+		self.ptrMessageDeleteByUserId(self.idGeneral)
+		mess = self.ptrMessageGetAllFromUserId(self.idGeneral)
+		if len(mess) > 0:
+			self.display('test_messageDeleteByUserId', 'Erreur perso...2', 1)
+			return ""
+		self.display('test_messageDeleteByUserId', "")
+
+	def test_messageGetAllFromUserId(self):
+		self.ptrMessageDeleteByUserId(self.idGeneral)
+		self.ptrMessageCreate(self.idGeneral, 'Bite', datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+		mess = self.ptrMessageGetAllFromUserId(self.idGeneral)
+		if len(mess) == 0:
+			self.display('test_messageGetAllFromUserId', 'Erreur perso...2', 1)
+			return ""
+		self.display('test_messageGetAllFromUserId', "")
+
+	def test_messageById(self):
+		self.ptrMessageDeleteByUserId(self.idGeneral)
+		self.ptrMessageCreate(self.idGeneral, 'Bite', datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+		mess = self.ptrMessageGetAllFromUserId(self.idGeneral)
+		idMess = mess[0][0]
+		newMess = self.ptrMessageById(idMess)
+		if newMess[2] == mess[0][2]:
+			self.display('test_messageById', "")
+			return ""
+		self.display('test_messageById', 'Erreur perso...', 1)
+
+	def test_messageUpdateContentById(self):
+		self.ptrMessageDeleteByUserId(self.idGeneral)
+		self.ptrMessageCreate(self.idGeneral, 'Bite', datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+		mess = self.ptrMessageGetAllFromUserId(self.idGeneral)
+		idMess = mess[0][0]
+		self.ptrMessageUpdateContentById("New Content", idMess)
+		mess2 = self.ptrMessageGetAllFromUserId(self.idGeneral)
+		#print(mess2)
+		if mess[0][2] != mess2[0][2]:
+			self.display('test_messageUpdateContentById', "")
+			return ""
+		self.display('test_messageUpdateContentById', 'Erreur perso...', 1)
+
+	def test_messageDeleteById(self):
+		self.ptrMessageDeleteByUserId(self.idGeneral)
+		self.ptrMessageCreate(self.idGeneral, 'Bite', datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+		self.ptrMessageDeleteById(self.idGeneral)
+		m = self.ptrMessageById(self.idGeneral)
+		#print(m)
+		if m is not None:
+			self.display('test_messageDeleteById', 'Erreur perso...', 1)
+			return ""
+		self.display('test_messageDeleteById', "")
+
+	def test_messageGetAllFromUserIdOrder(self):
+		self.ptrMessageDeleteByUserId(self.idGeneral)
+		self.ptrMessageCreate(self.idGeneral, 'Bite', datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+		mess = self.ptrMessageGetAllFromUserIdOrder(self.idGeneral)
+		if len(mess) == 0:
+			self.display('test_messageGetAllFromUserIdOrder', 'Erreur perso...2', 1)
+			return ""
+		self.display('test_messageGetAllFromUserIdOrder', "")
+
+	def test_likeMsgCount(self):
+		a1 = self.ptrLikeMsgCount()
+		self.ptrLikeMsgCreate(self.idGeneral, self.idGeneral, datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+		a2 = self.ptrLikeMsgCount()
+		if a1 == (a2 - 1):
+			self.display('test_likeMsgCount', "")
+			return ""
+		self.display('test_likeMsgCount', 'Erreur perso...', 1)
+
+	def test_likeMsgDelete(self):
+		a1 = self.ptrLikeMsgCount()
+		self.ptrLikeMsgCreate(self.idGeneral, self.idGeneral, datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+		self.ptrLikeMsgDelete(self.idGeneral, self.idGeneral)
+		a2 = self.ptrLikeMsgCount()
+		if a1 != a2:
+			self.display('test_likeMsgDelete', "")
+			return ""
+		self.display('test_likeMsgDelete', 'Erreur perso...', 1)
+
+	def test_likeMsgCreate(self):
+		a1 = self.ptrLikeMsgCount()
+		#print(a1)
+		self.ptrLikeMsgCreate(self.idGeneral, self.idGeneral, datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+		a2 = self.ptrLikeMsgCount()
+		#print(a2)
+		if a1 == (a2 - 1):
+			self.display('test_likeMsgCreate', "")
+			return ""
+		self.display('test_likeMsgCreate', 'Erreur perso...', 1)
+
+	def test_zDrop(self):
+		self.ptrMessageDeleteByUserId(self.idGeneral)
+		self.cleanEmail(self.emailGeneral)
+		self.ptrLikeMsgDelete(self.idGeneral, self.idGeneral)
+		self.display('test_zDrop', "   Drop all test")
+
+	def cleanEmail(self, email):
+		u = self.ptrUserByEmail(email)
+		if u is not None:
+			self.ptrUserDelete(email)
+			self.cleanEmail(email)
+
+	def display(self, name, value, error = 0):
+		sys.stdout.write('\033[32m-------------------------------------\033[0m\n')
+		if error == 1:
+			sys.stdout.write(" \033[30;41m ")
+			sys.stdout.write(name)
+			sys.stdout.write(" \033[0m \n")
+		else:
+			sys.stdout.write(" \033[30;42m ")
+			sys.stdout.write(name)
+			sys.stdout.write(" \033[0m \n")
+		if self.debug != False:
+			if len(value) > 0:
+				print(value)
+		sys.stdout.write(' \033[31m-------------------------------------\033[0m\n')
+if __name__ == '__main__':
+	unittest.main()
+
+
+#JVIENS DE FINI LE LIKE MSG IL FAUT FAIRE LE LIKE CMT & COMMENT
